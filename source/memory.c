062c798faa3ea4cf61dd6d6f15f65100cb6c1350
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2022-01-14 13:51:51 +0000

    Add closure

--- source/memory.c
+++ source/memory.c
@@ -31,6 +31,16 @@ static void freeObject(Obj* object) {
       FREE(ObjString, object);
       break;
     }
+    case OBJ_UPVALUE:
+      FREE(ObjUpvalue, object);
+      break;
+    case OBJ_CLOSURE: {
+      ObjClosure* closure = (ObjClosure*)object;
+      FREE_ARRAY(ObjUpvalue*, closure->upvalues,
+                 closure->upvalueCount);
+      FREE(ObjClosure, object);
+      break;
+    }
   }
 }
 
